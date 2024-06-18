54f259cff76ba13d03be1b25f1ec9ab8c3015ac5
Amine Aouragh <aouraghamine406@gmail.com>
2024-06-18 17:27:32 +0200

    Aanpassingen

--- app/src/main/java/com/example/weerapphobbyclub/MainActivity.java
+++ app/src/main/java/com/example/weerapphobbyclub/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.weerapphobbyclub;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -25,6 +27,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,8 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
     private static final String TAG = "MainActivity";
+    private static final String PREFS_NAME = "UserPrefs";
+    private static final String USERS_KEY = "users";
 
     private HashMap<String, String> userDatabase = new HashMap<>();
+    private boolean isLoggedIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,74 +75,73 @@ public class MainActivity extends AppCompatActivity {
         weatherTextView = findViewById(R.id.weatherTextView);
         nameTextView = findViewById(R.id.nameTextView);
 
+        loadUserData();
+
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = registerUsernameEditText.getText().toString();
-                String password = registerPasswordEditText.getText().toString();
-
-                if (username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Vul alle velden in", Toast.LENGTH_SHORT).show();
-                } else if (userDatabase.containsKey(username)) {
-                    Toast.makeText(MainActivity.this, "Gebruikersnaam is al geregistreerd", Toast.LENGTH_SHORT).show();
-                } else {
-                    userDatabase.put(username, password);
-                    Toast.makeText(MainActivity.this, "Registratie succesvol", Toast.LENGTH_SHORT).show();
-                    registerUsernameEditText.setVisibility(View.GONE);
-                    registerPasswordEditText.setVisibility(View.GONE);
-                    registerButton.setVisibility(View.GONE);
-
-                    usernameEditText.setVisibility(View.VISIBLE);
-                    passwordEditText.setVisibility(View.VISIBLE);
-                    loginButton.setVisibility(View.VISIBLE);
-                }
+                handleRegister();
             }
         });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = usernameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-
-                if (username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Vul alle velden in", Toast.LENGTH_SHORT).show();
-                } else if (!userDatabase.containsKey(username) || !userDatabase.get(username).equals(password)) {
-                    Toast.makeText(MainActivity.this, "Onjuiste gebruikersnaam of wachtwoord", Toast.LENGTH_SHORT).show();
-                } else {
-                    usernameEditText.setVisibility(View.GONE);
-                    passwordEditText.setVisibility(View.GONE);
-                    loginButton.setVisibility(View.GONE);
-
-                    cityEditText.setVisibility(View.VISIBLE);
-                    getWeatherButton.setVisibility(View.VISIBLE);
-                    weatherTextView.setVisibility(View.VISIBLE);
-                }
+                handleLogin();
             }
         });
 
         getWeatherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String city = cityEditText.getText().toString();
-                if (!city.isEmpty()) {
-                    getWeatherData(city);
-                } else {
-                    Toast.makeText(MainActivity.this, "Voer een stad in", Toast.LENGTH_SHORT).show();
-                }
+                handleGetWeather();
             }
         });
 
         if (savedInstanceState != null) {
-            registerUsernameEditText.setVisibility(savedInstanceState.getInt("registerUsernameVisibility"));
-            registerPasswordEditText.setVisibility(savedInstanceState.getInt("registerPasswordVisibility"));
-            registerButton.setVisibility(savedInstanceState.getInt("registerButtonVisibility"));
-            usernameEditText.setVisibility(savedInstanceState.getInt("usernameVisibility"));
-            passwordEditText.setVisibility(savedInstanceState.getInt("passwordVisibility"));
-            loginButton.setVisibility(savedInstanceState.getInt("loginButtonVisibility"));
-            cityEditText.setVisibility(savedInstanceState.getInt("cityEditTextVisibility"));
-            getWeatherButton.setVisibility(savedInstanceState.getInt("getWeatherButtonVisibility"));
-            weatherTextView.setVisibility(savedInstanceState.getInt("weatherTextViewVisibility"));
+            restoreInstanceState(savedInstanceState);
+        }
+    }
+
+    private void restoreInstanceState(Bundle savedInstanceState) {
+    }
+
+    private void handleRegister() {
+        String username = registerUsernameEditText.getText().toString().trim();
+        String password = registerPasswordEditText.getText().toString().trim();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Vul alle velden in", Toast.LENGTH_SHORT).show();
+        } else if (userDatabase.containsKey(username)) {
+            Toast.makeText(MainActivity.this, "Gebruikersnaam is al geregistreerd", Toast.LENGTH_SHORT).show();
+        } else {
+            userDatabase.put(username, password);
+            saveUserData();
+            Toast.makeText(MainActivity.this, "Registratie succesvol", Toast.LENGTH_SHORT).show();
+            showLoginFields();
+        }
+    }
+
+    private void handleLogin() {
+        String username = usernameEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Vul alle velden in", Toast.LENGTH_SHORT).show();
+        } else if (!userDatabase.containsKey(username) || !userDatabase.get(username).equals(password)) {
+            Toast.makeText(MainActivity.this, "Onjuiste gebruikersnaam of wachtwoord", Toast.LENGTH_SHORT).show();
+        } else {
+            isLoggedIn = true;
+            showWeatherFields();
+        }
+    }
+
+    private void handleGetWeather() {
+        String city = cityEditText.getText().toString().trim();
+        if (!city.isEmpty()) {
+            getWeatherData(city);
+        } else {
+            Toast.makeText(MainActivity.this, "Voer een stad in", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -156,26 +161,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
-                    WeatherResponse weatherResponse = response.body();
-                    double temperatureCelsius = weatherResponse.main.temp - 273.15;
-                    String temperatureFormatted = String.format("%.2f", temperatureCelsius);
-
-                    String weatherInfo = "Temperatuur: " + temperatureFormatted + "°C\n" +
-                            "Luchtvochtigheid: " + weatherResponse.main.humidity + "%\n" +
-                            "Wind Directie: " + convertDegreeToDirection(weatherResponse.wind.deg) + "\n" +
-                            "Wind Snelheid: " + weatherResponse.wind.speed + " m/s\n" +
-                            "Regen: " + (weatherResponse.weather[0].main.equals("Rain") ? "Ja" : "Nee");
-                    weatherTextView.setText(weatherInfo);
-                    weatherTextView.setVisibility(View.VISIBLE); // Ensure weatherTextView is set to visible
+                    displayWeatherData(response.body());
                 } else {
-                    Toast.makeText(MainActivity.this, "Fout bij het ophalen van de gegevens", Toast.LENGTH_SHORT).show();
-                    Log.e(TAG, "Response code: " + response.code());
-                    Log.e(TAG, "Response message: " + response.message());
-                    try {
-                        Log.e(TAG, "Response error body: " + response.errorBody().string());
-                    } catch (Exception e) {
-                        Log.e(TAG, "Fout bij het lezen van de foutboodschap", e);
-                    }
+                    showError(response);
                 }
             }
 
@@ -187,11 +175,73 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void displayWeatherData(WeatherResponse weatherResponse) {
+        double temperatureCelsius = weatherResponse.main.temp - 273.15;
+        String temperatureFormatted = String.format("%.2f", temperatureCelsius);
+
+        String weatherInfo = "Temperatuur: " + temperatureFormatted + "°C\n" +
+                "Luchtvochtigheid: " + weatherResponse.main.humidity + "%\n" +
+                "Wind Directie: " + convertDegreeToDirection(weatherResponse.wind.deg) + "\n" +
+                "Wind Snelheid: " + weatherResponse.wind.speed + " m/s\n" +
+                "Regen: " + (weatherResponse.weather[0].main.equals("Rain") ? "Ja" : "Nee");
+        weatherTextView.setText(weatherInfo);
+        weatherTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showError(Response<WeatherResponse> response) {
+        Toast.makeText(MainActivity.this, "Fout bij het ophalen van de gegevens", Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "Response code: " + response.code());
+        Log.e(TAG, "Response message: " + response.message());
+        try {
+            Log.e(TAG, "Response error body: " + response.errorBody().string());
+        } catch (Exception e) {
+            Log.e(TAG, "Fout bij het lezen van de foutboodschap", e);
+        }
+    }
+
     private String convertDegreeToDirection(float degree) {
         String[] directions = {"N", "NNO", "NO", "ONO", "O", "OZO", "ZO", "ZZO",
                 "Z", "ZZW", "ZW", "WZW", "W", "WNW", "NW", "NNW"};
         int index = Math.round(degree / 22.5f) % 16;
         return directions[index];
+    }
+
+    private void showLoginFields() {
+        registerUsernameEditText.setVisibility(View.GONE);
+        registerPasswordEditText.setVisibility(View.GONE);
+        registerButton.setVisibility(View.GONE);
+
+        usernameEditText.setVisibility(View.VISIBLE);
+        passwordEditText.setVisibility(View.VISIBLE);
+        loginButton.setVisibility(View.VISIBLE);
+    }
+
+    private void showWeatherFields() {
+        usernameEditText.setVisibility(View.GONE);
+        passwordEditText.setVisibility(View.GONE);
+        loginButton.setVisibility(View.GONE);
+
+        cityEditText.setVisibility(View.VISIBLE);
+        getWeatherButton.setVisibility(View.VISIBLE);
+        weatherTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void saveUserData() {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(userDatabase);
+        editor.putString(USERS_KEY, json);
+        editor.apply();
+    }
+
+    private void loadUserData() {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = prefs.getString(USERS_KEY, "");
+        if (!json.isEmpty()) {
+            userDatabase = gson.fromJson(json, HashMap.class);
+        }
     }
 
     @Override
@@ -206,11 +256,17 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt("cityEditTextVisibility", cityEditText.getVisibility());
         outState.putInt("getWeatherButtonVisibility", getWeatherButton.getVisibility());
         outState.putInt("weatherTextViewVisibility", weatherTextView.getVisibility());
+        outState.putBoolean("isLoggedIn", isLoggedIn);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        restoreVisibilityState(savedInstanceState);
+        isLoggedIn = savedInstanceState.getBoolean("isLoggedIn");
+    }
+
+    private void restoreVisibilityState(Bundle savedInstanceState) {
         registerUsernameEditText.setVisibility(savedInstanceState.getInt("registerUsernameVisibility"));
         registerPasswordEditText.setVisibility(savedInstanceState.getInt("registerPasswordVisibility"));
         registerButton.setVisibility(savedInstanceState.getInt("registerButtonVisibility"));
